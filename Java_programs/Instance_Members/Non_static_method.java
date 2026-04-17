95690bdb8ba21da6fe39de88a6345103f9f568dd
Aman <amanv9617@gmail.com>
2026-04-17 10:26:12 +0530

    add a non-static full concept program

--- Java_programs/Instance_Members/Non_static_method.java
+++ Java_programs/Instance_Members/Non_static_method.java
@@ -1,10 +1,22 @@
 public class Non_static_method {
+    static int num=0;
+     int num2 =23;
+
+    static void staticShow(){
+        System.out.println("static method runs");
+    }
         void show(){
-            System.out.println("non static method is running");
+            System.out.println("non-static method runs");
+            System.out.println(num);
+            staticShow();
         }
     public static void main(String[] args) {
         Non_static_method ma =new Non_static_method();
         ma.show();
+        // ma.staticShow();     //it will also run
+        // Non_static_method.staticShow();
+
+        
     }
 }
 
