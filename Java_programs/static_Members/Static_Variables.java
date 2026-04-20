cd57cc80b8ae4b74482671f76836f1d1edef8fcc
Aman <amanv9617@gmail.com>
2026-04-20 12:44:16 +0530

    add more files

--- Java_programs/static_Members/Static_Variables.java
+++ Java_programs/static_Members/Static_Variables.java
@@ -1,6 +1,9 @@
 public class Static_Variables {
-    
+    static String vue="js_framework";
+
     public static void main(String[] args) {
-        System.out.println("why run ");
+        
+        Static_Variables sv =new Static_Variables();
+        System.out.println(Static_Variables.vue);
     }
 }
